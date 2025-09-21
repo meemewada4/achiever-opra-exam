f212c012632b02d09f850c6048d66582226bf628
meemewada4 <66734691+meemewada4@users.noreply.github.com>
2025-09-21 13:26:33 +1200

    Rename Xcode project file to remove spaces

--- Achiever/ViewController.swift
+++ Achiever/ViewController.swift
@@ -1,19 +1,42 @@
-//
-//  ViewController.swift
-//  Achiever
-//
-//  Created by admin on 15/09/25.
-//
-
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    let webView = WKWebView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        // UI changes must happen on main thread
+        view.addSubview(webView)
+        webView.frame = view.bounds // or use Auto Layout
+
+        // Create URL (cheap operation)
+        guard let url = URL(string: "https://app.achieveropraexam.com/") else {
+            return
+        }
+
+        // Do ANY heavy or blocking work on a background queue
+        DispatchQueue.global(qos: .userInitiated).async {
+            // Example of heavy work you might do off the main thread:
+            // - Synchronously downloading something (not recommended),
+            // - Checking a local file,
+            // - Preparing/processing data
+
+            // Here we only create the URLRequest (this is cheap),
+            // but if you had blocking work, do it here.
+            let request = URLRequest(url: url)
+
+            // Now return to the main thread to update UI (load the web view)
+            DispatchQueue.main.async {
+                self.webView.load(request)
+            }
+        }
     }
 
-
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        webView.frame = view.bounds
+    }
 }
 
